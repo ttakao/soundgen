476cea4e617ba26ac6a3488d363706995838d0d3
Tsukasa Takao <tsukasa.takao@gmail.com>
2023-03-18 11:02:07 +0900

    First Release

---
+++ tuningfork.js
@@ -0,0 +1,19 @@
+Window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
+var oscillator;
+var isPlaying = false; // during play, true
+
+document.getElementById("play").addEventListener("click", () => {
+    if (isPlaying) return;
+    oscillator = ctx.createOscillator();
+    oscillator.type = "sin";
+    
+    var freqText = document.getElementById("freq").value;
+    
+    var freqNumber = Number(freqText);
+    alert(freqNumber);       
+    oscillator.frequency.setValueAtTime(freqNumber, ctx.currentTime);
+    oscillator.connect(ctx.destination);
+    oscilator.start();
+    isPlaying = true;
+}
