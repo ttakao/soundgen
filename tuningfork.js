316d71119d4e9019e981ebaf068b623ef6652570
Tsukasa Takao <tsukasa.takao@gmail.com>
2023-03-19 00:15:48 +0900

    First Release

--- tuningfork.js
+++ tuningfork.js
@@ -1,37 +1,50 @@
-Window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const ctx = new AudioContext();
-var oscillator;
+try{
+    //Window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    var ctx = new (window.AudioContext|| window.webkitAudioContext)();
+;
+} catch (e) {
+    alert('This browser does not work.');
+}
+
 var isPlaying = false; // during play, true
+var oscillator;
 
 document.getElementById("play").addEventListener("click", playSound);
 document.getElementById("stop").addEventListener("click", stopSound);
                                                  
 function playSound() {
+    console.log("play pushed.");
     if (isPlaying) return;
-    oscillator = ctx.createOscillator(); // oscillator must be made by each play.
-  
+
+    console.log(ctx.state);
+    oscillator = ctx.createOscillator(); //  oscillator must be made by each play.
+   
     var freqText = document.getElementById("freq").value; // get desired frequency.
     if (freqText == "") return;
+
     var freqNumber = Number(freqText);
+
+    oscillator.frequency.value = freqNumber;
 
     const waveforms = document.getElementsByName("waveform"); // get desired waveform.
     for (var i=0; i < waveforms.length; i++){
       if (waveforms[i].checked) {
          oscillator.type = waveforms[i].value;
-         console.log(oscillator.type);
          break;
       }     
     }
-  
-    oscillator.frequency.setValueAtTime(freqNumber, ctx.currentTime);
+
     oscillator.connect(ctx.destination);
-    oscillator.start();
+    oscillator.start(0);
     isPlaying = true;
 }
 
 function stopSound() {
     if (isPlaying){
-      oscillator.stop();
-      isPLaying = false;
+      oscillator.stop(0);
+      ctx.resume();
+      isPlaying = false;
     }
+    console.log("stop pushed");
 }
